b8953c516ad3bd7d0fb90a5b691e27c317b2431c
emredogan-cloud <ed897854@gmail.com>
2026-05-05 14:49:27 +0300

    refactor: phase 92 - rename package to globally unique com.emredogan.formaifit

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -28,7 +28,7 @@ val keystoreProperties = Properties().apply {
 }
 
 android {
-    namespace = "com.formai.app"
+    namespace = "com.emredogan.formaifit"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -47,7 +47,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.formai.app"
+        applicationId = "com.emredogan.formaifit"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         // Phase 77 · pinned to API 24 because newer ML Kit / MediaPipe
